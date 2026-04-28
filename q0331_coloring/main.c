c1815f17e2a39b583668bb8746f005a326e3a720
Joel Ting <joelccting@gmail.com>
2026-04-28 10:48:37 +0800

    0331

--- q0331_coloring/main.c
+++ q0331_coloring/main.c
@@ -1,6 +1,17 @@
+/* coloring: greedy
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+int a[50];
+
+int cmp(const void *a, const void *b)
+{
+    int *aa = (int *)a;
+    int *bb = (int *)b;
+    return (*aa > *bb);
+}
 
 int main()
 {
@@ -9,15 +20,25 @@ int main()
 
     for (int i = 0; i < t; ++i)
     {
-        scanf(" %d", &n);
-        int *a = (int *)malloc(sizeof(int) * n);
+        scanf("%d", &n);
+
         memset(a, 0, sizeof(int) * n);
 
         for (int j = 0; j < n; ++j)
-            scanf(" %d", a + j);
+            scanf("%d", a + j);
 
-
-        free(a);
+        qsort(a, n, sizeof(int), cmp);
+#if 0
+        for (int j = 0; j < n; ++j)
+            printf("%d ", a[j]);
+        printf("\n");
+#endif
+        int sum = 0, left = 0, right = n - 1;
+        while (right > left)
+        {
+            sum += (a[right--] - a[left++]);
+        }
+        printf("%d\n", sum);
     }
     return 0;
 }
