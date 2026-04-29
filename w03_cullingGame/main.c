e895115066c674ed6c449a9cf778ba0c6f33adeb
Joel Ting <joelccting@gmail.com>
2026-04-30 00:17:01 +0800

    Redo w03

--- w03_cullingGame/main.c
+++ w03_cullingGame/main.c
@@ -1,5 +1,55 @@
 #include <stdio.h>
 
+#include <stdio.h>
+/* @param arr    The input array (1, 2, 3, 4, 5)
+ * @param data   Temporary array to store current combination
+ * @param start  Starting index in arr[]
+ * @param end    Ending index in arr[]
+ * @param index  Current index in data[]
+ * @param depth  Size of combination to be printed
+ */
+void selection(int *arr, int *data, int start, int end, int index, int depth)
+{
+    if (index == depth)
+    {
+        for (int i = 0; i < depth; ++i)
+            printf("%d ", data[i]);
+        printf("\n");
+        return;
+    }
+
+    /* end - i + 1 >= depth - index
+     * stops the loop if there aren't enough numbers left in the array to finish the combination
+     */
+    for (int i = start; i <= end && end - i + 1 >= depth - index; ++i)
+    {
+        // printf("index=%d, i=%d\n", index, i);
+        data[index] = arr[i];
+        selection(arr, data, i + 1, end, index + 1, depth);
+    }
+}
+
+/*culling game*/
+void culling(void)
+{
+    int n, k;
+    int a[15], d[15];
+    scanf("%d%d", &n, &k);
+    for (int i = 0; i < n; ++i)
+    {
+        a[i] = i + 1;
+    }
+
+    selection(a, d, 0, n - 1, 0, k);
+}
+
+int main()
+{
+    culling();
+    return 0;
+}
+
+#if 0
 void generate(int n, int k, int start, int depth, int c[])
 {
     if (depth == k)
@@ -30,3 +80,4 @@ int main()
 
     return 0;
 }
+#endif
